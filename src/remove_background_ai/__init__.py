23d762eabdfdf9ed06115a8e31d7b65367781d2b
Nick Powell <184579+nmpowell@users.noreply.github.com>
2026-09-24 06:07:14 +0100

    Export the library's public interface from the package

--- src/remove_background_ai/__init__.py
+++ src/remove_background_ai/__init__.py
@@ -1,3 +1,28 @@
-from .core import remove_background
+"""Remove image backgrounds with OpenAI's GPT Image 2.5."""
 
-__all__ = ["remove_background"]
+from .core import remove_background
+from .editor import ImageEditor, OpenAIImageEditor
+from .errors import EditError, InvalidInputError, RemoveBackgroundError
+from .models import (
+    Cutout,
+    EditRequest,
+    EditResponse,
+    ImageSize,
+    RemovalOptions,
+    Usage,
+)
+
+__all__ = [
+    "Cutout",
+    "EditError",
+    "EditRequest",
+    "EditResponse",
+    "ImageEditor",
+    "ImageSize",
+    "InvalidInputError",
+    "OpenAIImageEditor",
+    "RemovalOptions",
+    "RemoveBackgroundError",
+    "Usage",
+    "remove_background",
+]
